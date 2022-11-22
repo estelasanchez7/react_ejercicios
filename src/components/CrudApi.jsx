061ab59d8d6bb6983e043266ff0cf47984837c53
estelasanchez7 <stelasv7@gmail.com>
2022-11-22 10:03:35 +0100

    Peticiones_HTTP_API

--- src/components/CrudApi.jsx
+++ src/components/CrudApi.jsx
@@ -6,6 +6,7 @@ import CrudTable from "./CrudTable";
 const CrudApi = () => {
   const [db, setDb] = useState([]);
   const [dataToEdit, setDataToEdit] = useState(null);
+  const [error, setError] = useState(null);
 
   let api = helpHttp();
   let url = "http://localhost:5000/harry_potter";
@@ -13,24 +14,51 @@ const CrudApi = () => {
   useEffect(() => {
     api.get(url).then((res) => {
       //console.log(res);
-      if(!res.err){
-        setDb(res)
-      }else{
-        setDb(null)
+      if (!res.err) {
+        setDb(res);
+      } else {
+        setDb(null);
       }
     });
-  }, [api,url]);
+  }, []);
 
   const createData = (data) => {
     data.id = Date.now();
     //console.log(data)
-    setDb([...db, data]);
+
+    let options = {
+      body: data,
+      headers: { "content-type": "application/json" },
+    };
+
+    api.post(url, options).then((res) => {
+      console.log(res);
+      if (!res.err) {
+        setDb([...db, res]);
+      } else {
+        setError(res);
+      }
+    });
   };
 
   const updateData = (data) => {
-    let newData = db.map((el) => (el.id === data.id ? data : el));
+    let endpoint = `${url}/${data.id}`;
+    //console.log(endpoint)
 
-    setDb(newData);
+    let options = {
+      body: data,
+      headers: { "content-type": "application/json" },
+    };
+
+    api.put(endpoint, options).then((res) => {
+      //console.log(res) ;
+      if (!res.err) {
+        let newData = db.map((el) => (el.id === data.id ? data : el));
+        setDb(newData);
+      } else {
+        setError(res);
+      }
+    });
   };
 
   const deleteData = (id) => {
@@ -39,8 +67,19 @@ const CrudApi = () => {
     );
 
     if (isDelete) {
-      let newData = db.filter((el) => el.id !== id);
-      setDb(newData);
+      let endpoint = `${url}/${id}`;
+      let options = {
+        headers: { "content-type": "application/json" },
+      };
+
+      api.del(endpoint, options).then((res) => {
+        if (!res.err) {
+          let newData = db.filter((el) => el.id !== id);
+          setDb(newData);
+        } else {
+          setError(res);
+        }
+      });
     } else {
       return;
     }
