f0402c2d8125ed18e352a9093ee4b386d79993ba
estelasanchez7 <stelasv7@gmail.com>
2022-11-25 15:54:46 +0100

    ValidacionFormularios_DefinicionComponentes_Logica

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 /*no es necesario mandar a llamar a react porque como es un hook personalizado solo está devolviendo lógica*/
 
 export const useFetch = (url) => {
