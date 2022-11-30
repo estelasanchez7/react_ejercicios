9c7051371b29c75537619c00251970cee86530db
estelasanchez7 <stelasv7@gmail.com>
2022-11-30 21:25:37 +0100

    Portals

--- src/components/Modals.jsx
+++ src/components/Modals.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { useModal } from "../hooks/useModal";
 import Modal from "./Modal";
+import ModalPortal from "./ModalPortal";
 import ContactForm from "./ContactForm"
 
 const Modals = () => {
@@ -27,16 +28,18 @@ const Modals = () => {
         <p>Hola este es el contenido de mi modal 2</p>
         <img src="https://placeimg.com/400/400/nature" alt="NAture" />
       </Modal>
+
       <button onClick={openModalContact}>Modal Contacto</button>
       <Modal isOpen={isOpenContact} closeModal={closeModalContact}>
         <ContactForm/>
       </Modal>
+
       <button onClick={openModalPortal}>Modal en Portal</button>
-      <Modal isOpen={isOpenPortal} closeModal={closeModalPortal}>
+      <ModalPortal isOpen={isOpenPortal} closeModal={closeModalPortal}>
       <h3>Modal en Portal</h3>
         <p>Este es el contenido de un modal que carga en otro nodo del DOM diferente a donde carga nuestra app de React, gracias a un Ract Portal</p>
         <img src="https://placeimg.com/400/400/tech" alt="Tech" />
-      </Modal>
+      </ModalPortal>
     </div>
   );
 };
