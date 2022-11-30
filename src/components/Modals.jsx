f6f60e6c63a4ad74c41c98c8f275ca49c90afe59
estelasanchez7 <stelasv7@gmail.com>
2022-11-30 14:15:44 +0100

    Portales

--- src/components/Modals.jsx
+++ src/components/Modals.jsx
@@ -8,6 +8,7 @@ const Modals = () => {
   const [isOpenModal1, openModal1, closeModal1] = useModal(false);
   const [isOpenModal2, openModal2, closeModal2] = useModal(false);
   const [isOpenContact, openModalContact, closeModalContact] = useModal(false)
+  const [isOpenPortal, openModalPortal, closeModalPortal] = useModal(false);
 
   return (
     <div>
@@ -29,6 +30,12 @@ const Modals = () => {
       <button onClick={openModalContact}>Modal Contacto</button>
       <Modal isOpen={isOpenContact} closeModal={closeModalContact}>
         <ContactForm/>
+      </Modal>
+      <button onClick={openModalPortal}>Modal en Portal</button>
+      <Modal isOpen={isOpenPortal} closeModal={closeModalPortal}>
+      <h3>Modal en Portal</h3>
+        <p>Este es el contenido de un modal que carga en otro nodo del DOM diferente a donde carga nuestra app de React, gracias a un Ract Portal</p>
+        <img src="https://placeimg.com/400/400/tech" alt="Tech" />
       </Modal>
     </div>
   );
