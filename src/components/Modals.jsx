e5cecdce9c7f73ba0490797be44b5a1cef7f4155
estelasanchez7 <stelasv7@gmail.com>
2022-11-30 12:27:22 +0100

    VentanaModal_Funcionalidad_ReutilizaciónComponente

--- src/components/Modals.jsx
+++ src/components/Modals.jsx
@@ -2,25 +2,34 @@
 import React from "react";
 import { useModal } from "../hooks/useModal";
 import Modal from "./Modal";
+import ContactForm from "./ContactForm"
 
 const Modals = () => {
-  const [isOpenModal1, openModal1, closeModal1] = useModal(false)
-  const [isOpenModal2, openModal2, closeModal2] = useModal(false)
+  const [isOpenModal1, openModal1, closeModal1] = useModal(false);
+  const [isOpenModal2, openModal2, closeModal2] = useModal(false);
+  const [isOpenContact, openModalContact, closeModalContact] = useModal(false)
+
   return (
     <div>
       <h2>Modales</h2>
-      <button>Modal 1</button>
-      <Modal>
+
+      <button onClick={openModal1}>Modal 1</button>
+      <Modal isOpen={isOpenModal1} closeModal={closeModal1}>
         <h3>Modal 1</h3>
         <p>Hola este es el contenido de mi modal 1</p>
         <img src="https://placeimg.com/400/400/animals" alt="Animals" />
       </Modal>
-      <button>Modal 2</button>
-      {/* <Modal>
+
+      <button onClick={openModal2}>Modal 2</button>
+      <Modal isOpen={isOpenModal2} closeModal={closeModal2}>
         <h3>Modal 2</h3>
         <p>Hola este es el contenido de mi modal 2</p>
         <img src="https://placeimg.com/400/400/nature" alt="NAture" />
-      </Modal> */}
+      </Modal>
+      <button onClick={openModalContact}>Modal Contacto</button>
+      <Modal isOpen={isOpenContact} closeModal={closeModalContact}>
+        <ContactForm/>
+      </Modal>
     </div>
   );
 };
